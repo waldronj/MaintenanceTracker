acb3b4d840775cb1d83d4f555facb6bd864736ec
Joshua Waldron <jwaldron@thejwal.com>
2013-07-03 22:38:26 -0500

    bootstrap added / jquery UI

--- MPGTracker/App_Start/BundleConfig.cs
+++ MPGTracker/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace MPGTracker
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery.js", 
+                        "~/Scripts/bootstrap.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
@@ -23,7 +25,7 @@ namespace MPGTracker
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css",  "~/Content/bootstrap-responsive.css", "~/Content/jquery-ui.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
