82601013624690ae232d606a4f9bc0df5abe6c91
Joshua Waldron <jwaldron@thejwal.com>
2013-07-04 13:23:49 -0500

    added site.css to bundles part of viewport fix

--- MPGTracker/App_Start/BundleConfig.cs
+++ MPGTracker/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace MPGTracker
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css",  "~/Content/bootstrap-responsive.css", "~/Content/jquery-ui.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css",  "~/Content/bootstrap-responsive.css", "~/Content/jquery-ui.css", "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
